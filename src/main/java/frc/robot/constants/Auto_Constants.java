cd1bd303ce49dd092df63ef9710b9b9dc1c262c7
melina77 <mathgiraffe77@gmail.com>
2023-03-16 16:45:02 -0700

    updated auto constatnts

--- src/main/java/frc/robot/constants/Auto_Constants.java
+++ src/main/java/frc/robot/constants/Auto_Constants.java
@@ -10,20 +10,13 @@ public class Auto_Constants {
 
     //Excuse my weird formatting I made it like this so I could see what I was doing better --JMoney
     public static final AutoData left_mobility = new AutoData           ("Left", "None", true, false);
-    public static final AutoData left_engage = new AutoData             ("Left", "None", false, true);
-    public static final AutoData left_mobility_engage = new AutoData    ("Left", "None", true, true);
-    public static final AutoData left_doubleScore = new AutoData        ("Left", "DoubleScore", true, false);
-    public static final AutoData left_doubleScore_engage = new AutoData ("Left", "DoubleScore", true, true);
     public static final AutoData middle_engage = new AutoData           ("Middle", "None", false, true);
     public static final AutoData right_mobility = new AutoData          ("Right", "None", true, false);
-    public static final AutoData right_engage = new AutoData            ("Right", "None", false, true);
-    public static final AutoData right_mobility_engage = new AutoData   ("Right", "None", true, true);
-    public static final AutoData right_doubleScore = new AutoData       ("Right", "DoubleScore", true, false);
-    public static final AutoData right_doubleScore_engage = new AutoData("Right", "DoubleScore", true, true);
+    public static final AutoData middle_mobility_engage = new AutoData          ("Middle", "None", true, true);
 
     public static final AutoData[] autoChoices = {
-        left_mobility, left_engage, left_mobility_engage, left_doubleScore, left_doubleScore_engage,
-        middle_engage,
-        right_mobility, right_engage, right_mobility_engage, right_doubleScore, right_doubleScore_engage
+        left_mobility,
+        middle_engage, middle_mobility_engage,
+        right_mobility,
     };
 }
