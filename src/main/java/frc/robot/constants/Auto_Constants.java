86a6025cad413e902f6ccf456cdb3ab0124194d0
JosephSwearingen <jhsweargn@gmail.com>
2023-03-16 17:50:51 -0700

    Forgot to delete auto constants

--- src/main/java/frc/robot/constants/Auto_Constants.java
+++ src/main/java/frc/robot/constants/Auto_Constants.java
@@ -1,22 +1,6 @@
 package frc.robot.constants;
 
-import frc.robot.objects.AutoData;
-
 public class Auto_Constants {
     public static final double maxVelo = 4;
     public static final double maxAccel = 3;
-
-    public static final int autoAmount = 11;
-
-    //Excuse my weird formatting I made it like this so I could see what I was doing better --JMoney
-    public static final AutoData left_mobility = new AutoData           ("Left", "None", true, false);
-    public static final AutoData middle_engage = new AutoData           ("Middle", "None", false, true);
-    public static final AutoData right_mobility = new AutoData          ("Right", "None", true, false);
-    public static final AutoData middle_mobility_engage = new AutoData          ("Middle", "None", true, true);
-
-    public static final AutoData[] autoChoices = {
-        left_mobility,
-        middle_engage, middle_mobility_engage,
-        right_mobility,
-    };
 }
